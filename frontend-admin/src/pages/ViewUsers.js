426afef5c4c92317274bbcea57377cc2d41ec207
Kollu Saketharam <sakethk4106@gmail.com>
2026-08-07 19:45:08 +0530

    Update ViewUsers.js

--- frontend-admin/src/pages/ViewUsers.js
+++ frontend-admin/src/pages/ViewUsers.js
@@ -21,7 +21,7 @@ const ViewUsers = () => {
 
       try {
         const res = await fetch(
-          `http://localhost:5000/api/admin/profile`,
+          `${process.env.REACT_APP_API_URL}/api/admin/profile`,
           {
             method: 'GET',
             headers: {
