d77e73e7fc24650d26b5ce2483ffbceb9693b471
Kollu Saketharam <sakethk4106@gmail.com>
2026-08-07 19:18:10 +0530

    Update ViewEvents.js

--- frontend-admin/src/platform/ViewEvents.js
+++ frontend-admin/src/platform/ViewEvents.js
@@ -10,7 +10,7 @@ const ViewEvents = () => {
         const fetchEvents = async () => {
             try {
                 const token = localStorage.getItem("token");
-                const response = await fetch("http://localhost:5000/api/superadmin/events", {
+                const response = await fetch("https://eventsphere-95n2.onrender.com/api/superadmin/events", {
                     headers: {
                         Authorization: `Bearer ${token}`
                     }
