fa2d678a26fefc22bbf7b13aefda83a8d7f56c4d
Kollu Saketharam <sakethk4106@gmail.com>
2026-08-07 19:15:42 +0530

    Update StaffLogin.js

--- frontend-admin/src/pages/StaffLogin.js
+++ frontend-admin/src/pages/StaffLogin.js
@@ -12,7 +12,7 @@ function StaffLogin() {
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/admin/login", {
+      const res = await fetch("https://eventsphere-95n2.onrender.com/api/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: email.trim().toLowerCase(), password }),
