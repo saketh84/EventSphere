e6d38ecf04d8eb45861057f6cce54d00146acd06
Kollu Saketharam <sakethk4106@gmail.com>
2026-08-07 19:36:28 +0530

    Update BrowseEvents.js

--- frontend/src/pages/BrowseEvents.js
+++ frontend/src/pages/BrowseEvents.js
@@ -36,7 +36,7 @@ function BrowseEvents() {
 
     const fetchEvents = async () => {
         try {
-            const response = await fetch('https://eventsphere-95n2.onrender.com/api/events/all');
+            const response = await fetch('${process.env.REACT_APP_API_URL}/api/events/all');
             const data = await response.json();
 
             const eventsArray = Array.isArray(data)
